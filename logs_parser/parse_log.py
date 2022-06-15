ea40874ba8b8e8b95885a496d6a2710c9a29a8b3
Sixte de Maupeou <sixtedemaupeou@gmail.com>
2022-06-15 12:27:26 +0200

    implement file lifecycle management

--- logs_parser/parse_log.py
+++ logs_parser/parse_log.py
@@ -8,7 +8,9 @@ from logs_parser.process_logs import generate_logfile_df, clean_df, enrich_logs_
 
 
 
-def download_files(minio_folder: str) -> None:
+def download_files(minio_folder: str, target_folder: str) -> None:
+    """Download all log files from Minio"""
+    os.makedirs(target_folder, exist_ok=True)
     log_keys = list_log_files(minio_folder)
     for log_key in log_keys:
         download_from_minio(log_key, log_key)
@@ -16,11 +18,16 @@ def download_files(minio_folder: str) -> None:
 
 
 async def record_daily_views(minio_folder: str) -> None:
-    download_files(minio_folder)
     logs_folder = f"./{minio_folder}"
+    download_files(minio_folder, logs_folder)
+
     all_log_files = os.listdir(logs_folder)
     df = pd.concat([generate_logfile_df(os.path.join(logs_folder, filename)) for filename in all_log_files], ignore_index=True)
     df = clean_df(df)
     df = enrich_logs_df(df)
     await write_logs_to_db(df)
-    # TODO: Cleanup logs folder
+
+    # Cleanup
+    for filename in all_log_files:
+        os.remove(os.path.join(logs_folder, filename))
+    os.rmdir(logs_folder)
